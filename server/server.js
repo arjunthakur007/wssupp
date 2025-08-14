6262d94e427bfdeab98ee630679272c0e50f2f66
ATarjun007 <atarjun2000@gmail.com>
2025-08-14 23:42:48 +0530

    server.js code change

--- server/server.js
+++ server/server.js
@@ -12,10 +12,16 @@ import { Server } from "socket.io";
 const app = express();
 const server = http.createServer(app);
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://quickchat-beige.vercel.app",
+];
+
 //Socket.io server initialize
 export const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://quickchat-beige.vercel.app"],
+    origin: allowedOrigins,
+    credentials: true,
   },
 });
 //Store online users
@@ -37,11 +43,6 @@ io.on("connection", (socket) => {
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
   });
 });
-
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://quickchat-beige.vercel.app",
-];
 
 //Middleware configuration
 app.use(express.json({ limit: "4mb" }));
