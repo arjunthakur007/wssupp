4522c7eac92eb705b65ea711459213e74cf21afd
ATarjun007 <atarjun2000@gmail.com>
2025-08-15 17:06:19 +0530

    authUser.js code change

--- server/middleware/authUser.js
+++ server/middleware/authUser.js
@@ -3,7 +3,7 @@ import User from "../models/User.js";
 
 const authUser = async (req, res, next) => {
   try {
-    const token  = req.headers.token;
+    const token = req.headers.token || req.headers.authorization?.split(" ")[1];
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     const user = await User.findById(decoded.userId).select("-password");
